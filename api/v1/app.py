e2df59b5775e012e8b54be9dc3d7d17b4e64987f
Fortunatekio <fortunatemugendi8@gmail.com>
2024-01-06 17:25:53 +0300

    Resolved PEP8 issues

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_app_context(exception):
-"""Closes the storage"""
-storage.close()
+    """Closes the storage"""
+    storage.close()
 
-if __name__ == "__main__":
-host = getenv('HBNB_API_HOST', '0.0.0.0')
-port = getenv('HBNB_API_PORT', 5000)
-app.run(host=host, port=port, threaded=True)
-
+    if __name__ == "__main__":
+        host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
