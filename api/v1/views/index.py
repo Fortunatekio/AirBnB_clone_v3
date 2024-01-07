2dc351cbfc6fda487ff66919fb90049c3f361f1a
Vincent Mugendi <vincentmwathi5@gmail.com>
2024-01-07 06:32:12 -0800

    Fixes to index.py importing jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+
+from flask import Flask, jsonify
 from api.v1.views import app_views
+
+app = Flask(__name__)
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of API """
     return jsonify({"status": "OK"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
