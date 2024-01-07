bc637b4aa7a41d5873a13c4371ca0486553e933b
Vincent Mugendi <vincentmwathi5@gmail.com>
2024-01-07 05:26:51 -0800

    Resolved python path and chnsges to app.py and its init file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from os import getenv
 from models import storage
+from models.state import State
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
 
@@ -12,7 +13,7 @@ def teardown_app_context(exception):
     """Closes the storage"""
     storage.close()
 
-    if __name__ == "__main__":
-        host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', 5000)
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
