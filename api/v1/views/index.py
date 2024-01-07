44f8d55de2a746d7d9668d4d3a0a5ddc8e3965ef
vincent-mugendi <vincentmwathi5@gmail.com>
2024-01-07 18:06:40 +0300

    Documentation for index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""This module defines routes for the API"""
 
 from flask import Flask, jsonify
 from api.v1.views import app_views
@@ -8,7 +9,7 @@ app = Flask(__name__)
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """ Status of API """
+    """Returns Status of API """
     return jsonify({"status": "OK"})
 
 
