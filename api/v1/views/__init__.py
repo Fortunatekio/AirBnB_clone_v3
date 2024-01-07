556f5b7126ba8b8a5066565c72ff80c2eca8342b
vincent-mugendi <vincentmwathi5@gmail.com>
2024-01-07 17:49:47 +0300

    fixed PEP8 erros for __init__.py files

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """ Blueprint for API """
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from models.state import State
 from models.place import *
@@ -11,3 +8,6 @@ from models.review import *
 from models.city import *
 from models.amenity import *
 from models.user import *
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
